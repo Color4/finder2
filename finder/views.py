35a46a14296c0fbf726541d66e16b1264a05d778
kang <78956287@qq.com>
2016-01-10 10:24:52 +0800

    sd

--- finder/views.py
+++ finder/views.py
@@ -1,7 +1,7 @@
 import datetime
 
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect,Http404
+from django.http import HttpResponse, HttpResponseRedirect,Http404, HttpResponseBadRequest
 from django.template import Context, Template
 from django.core.urlresolvers import reverse
 from .form import *
@@ -14,8 +14,10 @@ from captcha.models import CaptchaStore
 from captcha.helpers import captcha_image_url
 from django.http import JsonResponse
 import hashlib
-
-
+from wechat_sdk import WechatBasic
+from wechat_sdk.exceptions import ParseError
+from wechat_sdk.messages import TextMessage
+from django.views.decorators.csrf import csrf_exempt
 
 def add(request, a, b):  # /分割
     # a = request.GET['a']
@@ -180,26 +182,72 @@ def person(request):
 
 WEIXIN_TOKEN = 'xiaobaifinder'
 
-def weixin_check(request):
-    """
-    所有的消息都会先进入这个函数进行处理，函数包含两个功能，
-    微信接入验证是GET方法，
-    微信正常的收发消息是用POST方法。
-    """
-    if request.method == "GET":
-        signature = request.GET.get("signature", None)
-        timestamp = request.GET.get("timestamp", None)
-        nonce = request.GET.get("nonce", None)
-        echostr = request.GET.get("echostr", None)
-        token = WEIXIN_TOKEN
-        tmp_list = [token, timestamp, nonce]
-        tmp_list.sort()
-        tmp_str = "%s%s%s" % tuple(tmp_list)
-        tmp_str = tmp_str.encode('utf-8')
-        tmp_str = hashlib.sha1(tmp_str).hexdigest()
-        if tmp_str == signature:
-            return HttpResponse(echostr)
-        else:
-            return HttpResponse("error")
-    else:
-        return HttpResponse('error')
+# AppID(应用ID)wxaa5b23ee724df3c6
+# AppSecret(应用密钥)14e808be891c1e5007c1cfc92dac8ab5
+# Connection: close
+# Date: Sat, 09 Jan 2016 13:53:54 GMT
+# Content-Type: application/json; encoding=utf-8
+# Content-Length: 154
+# {
+#     "access_token": "bYV34mrZxrfH2k9nLwW4SgbWL1eTY_qVfsMKFbhEOrTYsuUWqIywd7GXKPpeA5NxxBsVcxYkRjAh3UaIDuwqWOm7gx0V03u7CyPTciAwX6sDNOfAHAVFG",
+#     "expires_in": 7200
+# }
+AppID = 'wxaa5b23ee724df3c6'
+AppSecret = '14e808be891c1e5007c1cfc92dac8ab5'
+
+# 实例化 WechatBasic
+wechat_instance = WechatBasic(
+    token=WECHAT_TOKEN,
+    appid=AppID,
+    appsecret=AppSecret
+)
+
+@csrf_exempt
+def weixin(request):
+    if request.method == 'GET':
+        # 检验合法性
+        # 从 request 中提取基本信息 (signature, timestamp, nonce, xml)
+        signature = request.GET.get('signature')
+        timestamp = request.GET.get('timestamp')
+        nonce = request.GET.get('nonce')
+
+        if not wechat_instance.check_signature(
+                signature=signature, timestamp=timestamp, nonce=nonce):
+            return HttpResponseBadRequest('Verify Failed')
+
+        return HttpResponse(
+            request.GET.get('echostr', ''), content_type="text/plain")
+
+
+    # 解析本次请求的 XML 数据
+    try:
+        wechat_instance.parse_data(data=request.body)
+    except ParseError:
+        return HttpResponseBadRequest('Invalid XML Data')
+
+    # 获取解析好的微信请求信息
+    message = wechat_instance.get_message()
+
+    # 关注事件以及不匹配时的默认回复
+    response = wechat_instance.response_text(
+        content = (
+            '感谢您的关注！\n回复【功能】两个字查看支持的功能，还可以回复任意内容开始聊天'
+            '\n【<a href="http://www.ziqiangxuetang.com">自强学堂手机版</a>】'
+            ))
+    if isinstance(message, TextMessage):
+        # 当前会话内容
+        content = message.content.strip()
+        if content == '功能':
+            reply_text = (
+                    '目前支持的功能：\n1. 关键词后面加上【教程】两个字可以搜索教程，'
+                    '比如回复 "Django 后台教程"\n'
+                    '2. 回复任意词语，查天气，陪聊天，讲故事，无所不能！\n'
+                    '还有更多功能正在开发中哦 ^_^\n'
+                    '【<a href="http://www.ziqiangxuetang.com">自强学堂手机版</a>】'
+                )
+        elif content.endswith('教程'):
+            reply_text = '您要找的教程如下：'
+
+        response = wechat_instance.response_text(content=reply_text)
+
+    return HttpResponse(response, content_type="application/xml")
