b8c7aa26b406f083117f07b58bf6d592f600f588
kang <78956287@qq.com>
2016-01-14 17:49:03 +0800

    media

--- web/urls.py
+++ web/urls.py
@@ -64,8 +64,8 @@ urlpatterns = [
 ]
 
 # if settings.DEBUG:
-# urlpatterns += [
-#     url(r'^media/(?P<path>.*)$', serve, {
-#         'document_root': settings.MEDIA_ROOT,
-#     }),
-# ]
+urlpatterns += [
+    url(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
