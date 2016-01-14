d07560e39670650460c04109d01622ddb526ed25
kang <78956287@qq.com>
2016-01-14 17:27:05 +0800

    revise media_root to media

--- web/settings.py
+++ web/settings.py
@@ -177,7 +177,9 @@ LOCALE_PATHS = (
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media_root")
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media_root")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 LOGIN_REDIRECT_URL='/'
 
 LOGIN_URL='/login'
