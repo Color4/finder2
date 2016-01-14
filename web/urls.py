3c61676b14c34fe78a7cb849524145cf93351d83
kang <78956287@qq.com>
2016-01-14 17:06:35 +0800

    revise media dir

--- web/urls.py
+++ web/urls.py
@@ -63,9 +63,9 @@ urlpatterns = [
 
 ]
 
-if settings.DEBUG:
-    urlpatterns += [
-        url(r'^media/(?P<path>.*)$', serve, {
-            'document_root': settings.MEDIA_ROOT,
-        }),
-   ]
+# if settings.DEBUG:
+urlpatterns += [
+    url(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
